2ba4627666754ba2cd1bb92b8293cca3f12ea3e0
bermalA <aratoglubermal@gmail.com>
2024-02-21 13:08:42 +0300

    swe 304 assignment base project

---
+++ src/test/java/com/example/swe304/Swe304ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.swe304;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Swe304ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
